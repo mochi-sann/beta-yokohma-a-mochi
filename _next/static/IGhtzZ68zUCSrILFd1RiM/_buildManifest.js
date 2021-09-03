50c09b720b1c0861028f6c59de83569505026123
mochi-sann <mochi-sann@users.noreply.github.com>
2021-09-03 06:27:17 +0000

    deploy: 3d85585e9e1a5950533c845b861bfb0b3139c432

--- _next/static/IGhtzZ68zUCSrILFd1RiM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7fdb448833b4d7be9356.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
