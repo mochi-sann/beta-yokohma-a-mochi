b6f578725046121dd8568bc8e2d31cb5d40679ca
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-03 15:34:32 +0900

    アイコンを追加

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,17 @@ export type Props = {
   children: React.ReactNode
 }
 import { NextSeo } from 'next-seo'
-
+import Head from 'next/head'
 const Layout: React.VFC<Props> = (props) => {
   return (
     <Container maxW="900px">
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://mochi-sann.github.io/beta-yokohma-a-mochi/icon.png"
+          type="image/x-icon"
+        />
+      </Head>
       <NextSeo
         title="奴らのいる工場から脱出 | プロジェクトN"
         description="A short description goes here."
