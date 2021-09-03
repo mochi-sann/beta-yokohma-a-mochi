e1503b26e8fa15b8a469898c73147556a921b5f5
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-03 15:21:18 +0900

    いろいろ設定

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,18 @@ import { Container } from '@chakra-ui/react'
 export type Props = {
   children: React.ReactNode
 }
+import { NextSeo } from 'next-seo'
 
 const Layout: React.VFC<Props> = (props) => {
-  return <Container maxW="900px">{props.children}</Container>
+  return (
+    <Container maxW="900px">
+      <NextSeo
+        title="奴らのいる工場から脱出 | プロジェクトN"
+        description="A short description goes here."
+      />
+      {props.children}
+    </Container>
+  )
 }
 
 export default Layout
