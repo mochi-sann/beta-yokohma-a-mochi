06e05e8587d8e2d19a76debe6a307627a3ea34b6
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-03 20:06:52 +0900

    いろいろ修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '../components/Layout'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+
+import Layout from '../components/Layout'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-        />
-      </Head>
       <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
