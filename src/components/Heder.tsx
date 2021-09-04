5df2e7edbaddcbf90bd20ae937fa91a774cfb2f8
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-04 21:58:33 +0900

    いろいろ追加

---
+++ src/components/Heder.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Flex, Spacer, Heading } from '@chakra-ui/react'
+
+const Heder: React.VFC = () => {
+  return (
+    <Flex p="1">
+      <Heading>たいとる</Heading>
+
+      <Spacer />
+    </Flex>
+  )
+}
+
+export default Heder
