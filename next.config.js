184f71621f1d7f8ece13d1fbb963fe29c5564fa9
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-06 14:44:33 +0900

    gh pagesの設定を変えた

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   trailingSlash: true,
-  assetPrefix: process.env.GITHUB_PAGES ? '/beta-yokohma-a-mochi' : '',
-  baseUrl: process.env.GITHUB_PAGES ? '/beta-yokohma-a-mochi' : '',
+  assetPrefix: process.env.BASE_PATH || '',
+  // basePath: process.env.BASE_PATH || '',
 }
