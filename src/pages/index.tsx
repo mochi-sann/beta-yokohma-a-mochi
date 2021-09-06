63018c672e1adb9b5963f83d91f26a983e5270f4
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-06 10:46:45 +0900

    正解不正解によって挙動を変えるhooksを作った

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import useNextPage from '../hooks/useNextPage'
+import { Button } from '@chakra-ui/react'
+
 export const Home: React.VFC = () => {
+  const { handleClick } = useNextPage({ nextPage: '/page1' })
   return (
     <div>
       <p>がぞう</p>
+      <Button
+        colorScheme="gray"
+        onClick={() => {
+          handleClick({ isCorrect: true, NotShowToast: true })
+        }}
+        // as="a"
+      >
+        {' '}
+        ゲームを始める
+      </Button>
     </div>
   )
 }
---
+++ src/pages/page1.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home: React.VFC = () => {
+  return (
+    <div>
+      <p>ページ2</p>
+    </div>
+  )
+}
+
+export default Home
