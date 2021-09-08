b19127288fff6462c37c25791548d4f06feebeea
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-08 10:32:33 +0900

    ひんtのを表示するとところを作った

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Problems from '~/src/components/Problems/Problems'
-
+import HintList from '~/src/components/HintList'
 export const Home: React.VFC = () => {
   return (
     <div>
       <p>いちもんめ</p>
+
       <Problems
         nextPage="/page2"
         correctList={[
@@ -23,6 +24,12 @@ export const Home: React.VFC = () => {
             correctText: 'aaa',
             type: 'text',
           },
+        ]}
+      />
+      <HintList
+        HintLists={[
+          { title: 'ひんと1', body: 'ああああああああああああああ' },
+          { title: 'ひんと2', body: 'Welcome' },
         ]}
       />
     </div>
