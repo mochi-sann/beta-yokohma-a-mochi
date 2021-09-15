bdd9ff937431cd54afd22e6f73b2f893d62dd532
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-15 10:58:01 +0900

    画像にBASE Pathを追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 module.exports = {
   trailingSlash: true,
   assetPrefix: process.env.BASE_PATH || '',
   basePath: process.env.BASE_PATH || '',
+  env: {
+    BASE_PATH: process.env.BASE_PATH,
+  },
 }
