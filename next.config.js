acd5da462fd919119c618df83591bacf845346fa
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-15 11:01:33 +0900

    画像のBASEPATHが正しく動作するようにした

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-// require('dotenv').config()
+require('dotenv').config()
 
 module.exports = {
   trailingSlash: true,
   assetPrefix: process.env.BASE_PATH || '',
   basePath: process.env.BASE_PATH || '',
+  publicRuntimeConfig: { urlPrefix: process.env.BASE_PATH || '' }, // ★コレ
+
   env: {
     BASE_PATH: process.env.BASE_PATH,
   },
