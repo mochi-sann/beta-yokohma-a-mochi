c47fcf28d45e78fa4483e01801870a5add2b5329
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-09-15 14:05:08 +0900

    Googlefontを有効化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <ChakraProvider theme={theme}>
       <Layout>
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
@@ -18,7 +18,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <link
             href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&display=swap"
             rel="stylesheet"
-          /> */}
+          />
 
           <meta
             name="viewport"
